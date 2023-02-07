d26b137253cda675f5833ccbda5c362295975778
crc6267 <cscrcapps@gmail.com>
2023-02-07 17:22:45 -0500

    Project skeleton

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          scaffoldBackgroundColor: Color(0xFF222233),
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              color: Color.fromARGB(255, 189, 216, 255),
+              fontSize: 30,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.bold,
+            ),
+            displayMedium: TextStyle(
+              color: Color.fromARGB(255, 189, 216, 255),
+              fontSize: 15,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Center(
+          child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to Get Up',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // Add your name here :)
+            Text(
+              'Created by...',
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
