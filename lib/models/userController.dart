b6283dda3711a1250b56c6c8c57f6c9a7f19acfc
crob17 <coleroberts103@gmail.com>
2023-02-22 13:35:38 -0500

    User class getters and setters

--- lib/models/userController.dart
+++ lib/models/userController.dart
@@ -9,4 +9,34 @@ class User {
       {required this.firstName,
       required this.lastName,
       required this.userName});
+
+  set firstName(String nameF) {
+    firstName = nameF;
+  }
+
+  set lastName(String nameL) {
+    lastName = nameL;
+  }
+
+  set userName(String nameU) {
+    userName = nameU;
+  }
+
+  String get firstname {
+    return firstName;
+  }
+
+  String get lastname {
+    return lastName;
+  }
+
+  String get username {
+    return userName;
+  }
 }
+
+List<User> sampleUsers = [
+  User(firstName: 'Cole', lastName: 'Roberts', userName: 'CROB'),
+  User(firstName: 'Cullin', lastName: 'Caps', userName: 'CCAP'),
+  User(firstName: 'John', lastName: 'Doe', userName: 'JDOE')
+];
