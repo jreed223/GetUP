fbb608a5c04c092127db8ea19d728b3a78ee0bad
crob17 <coleroberts103@gmail.com>
2023-02-23 11:47:32 -0500

    Added name to home page

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
             // Add your name here :)
             // Dont forget to add your name to the readme.md file
             Text(
-              'Created by Cullin Capps, Aaron Ayris, Lukas Becker',
+              'Created by Cullin Capps, Aaron Ayris, Lukas Becker, Cole Roberts',
               style: Theme.of(context).textTheme.displayMedium,
             ),
           ],
