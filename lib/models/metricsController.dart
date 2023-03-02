4d620c301f128ba40e8019857ae155a94868c401
unknown <jonathanreed52@hotmail.com>
2023-03-02 17:03:24 -0500

    Organized handler and created methods for calculation

--- lib/models/metricsController.dart
+++ lib/models/metricsController.dart
@@ -1,88 +1,64 @@
+import "goals.dart";
+
 ///Class holds data needed to create visualizations for goal Data
-class metricsData {
-  int goalsCompleted;
-  int totalGoals;
-  double actTime; //actual time spent in hours
-  double expTime; //expected time spent in hours
-  String date;
+class MetricsData {
+  double completionPrcnt;
+  double durationPrcnt;
+  double overallPrcnt;
+  double totalDuration;
 
   ///initializes data
-  metricsData(
-      {required this.goalsCompleted,
-      required this.totalGoals,
-      required this.actTime,
-      required this.expTime,
-      required this.date});
+  MetricsData(
+      {required this.completionPrcnt,
+      required this.durationPrcnt,
+      required this.overallPrcnt,
+      required this.totalDuration});
 
   ///Creates a list of objects containing data for each day of the week
-  static List<metricsData> dailyData = [
-    metricsData(
-        goalsCompleted: 1,
-        totalGoals: 4,
-        actTime: 1.0,
-        expTime: 3.0,
-        date: 'Mon'),
-    metricsData(
-        goalsCompleted: 3,
-        totalGoals: 5,
-        actTime: 2.0,
-        expTime: 2.0,
-        date: "Tues"),
-    metricsData(
-        goalsCompleted: 2,
-        totalGoals: 5,
-        actTime: 1.0,
-        expTime: 3.0,
-        date: "Wed"),
-    metricsData(
-        goalsCompleted: 6,
-        totalGoals: 6,
-        actTime: 4.0,
-        expTime: 4.0,
-        date: "Thurs"),
-    metricsData(
-        goalsCompleted: 4,
-        totalGoals: 5,
-        actTime: 3.0,
-        expTime: 4.0,
-        date: "Fri"),
-    metricsData(
-        goalsCompleted: 8,
-        totalGoals: 8,
-        actTime: 6.0,
-        expTime: 6.0,
-        date: "Sat"),
-    metricsData(
-        goalsCompleted: 5,
-        totalGoals: 7,
-        actTime: 3.0,
-        expTime: 4.0,
-        date: "Sun"),
+  static List<MetricsData> dailyData = [
+    MetricsData(
+      completionPrcnt: 85.00,
+      durationPrcnt: 44.00,
+      overallPrcnt: 55.00,
+      totalDuration: 60.00,
+    )
   ];
 }
 
-///Class used to calculate data received in the MetricsData class
-class metricsController {
-  double timePrcntg;
-  double completionPrcntg;
+///Class used to calculate and handle data received for the MetricsData class
+class MetricsController {}
 
-  metricsController({required this.timePrcntg, required this.completionPrcntg});
+double prcntTime(progress, duration) {
+  // method measures progress by comparing time spent to the expected time spent
+  double durationPrcnt = progress / duration;
+  return durationPrcnt;
+}
 
-  double prcntTime(actTime, expTime) {
-    // method measures progress by comparing time spent to the expected time spent
-    timePrcntg = actTime / expTime;
-    return timePrcntg;
+double prcntComplete(goalsCompleted, totalGoals) {
+  // method measures progress by comparing goals completed to the amount of goals created
+  double completionPrcnt = goalsCompleted / totalGoals;
+  return completionPrcnt;
+}
+
+double prcntOverall(completionPrcnt, durationPrcnt) {
+  double overallPrcnt = (completionPrcnt + durationPrcnt) / 2;
+  return overallPrcnt;
+}
+
+MetricsController sample = new MetricsController();
+void main() {
+  final List<Goal> _sampleGoals = [
+    LongTermGoal(title: 'Learn Flutter', duration: 20),
+    Goal(title: 'Go to grocery store'),
+    LongTermGoal(title: 'Read', duration: 5),
+    Goal(title: 'Go to the gym'),
+    LongTermGoal(title: 'Learn Dart', duration: 10),
+    Goal(title: 'Go to the dentist'),
+    LongTermGoal(title: 'Learn Python', duration: 15),
+  ];
+  for (var i = 0; i < _sampleGoals.length; i++) {
+    // TO DO
+    var currentGoal = _sampleGoals[i];
+    print(currentGoal);
   }
-
-  double prcntComplete(goalsCompleted, totalGoals) {
-    // method measures progress by comparing goals completed to the amount of goals created
-    completionPrcntg = goalsCompleted / totalGoals;
-    return completionPrcntg;
-  }
-
-  double get timeProgress =>
-      timePrcntg; //getter that retrieves percentage of progress based on time
-
-  double get completedProgress =>
-      completionPrcntg; //getter that retrieves percentage of goals fully completed
 }
