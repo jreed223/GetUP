43240979394f87af1bc31426b69f94366c536826
1-Pound <94070939+1-Pound@users.noreply.github.com>
2023-04-30 21:45:51 -0400

    added a delay to the notifyListener

--- lib/models/challenge.dart
+++ lib/models/challenge.dart
@@ -98,11 +98,10 @@ class Challenge extends Goal {
   ChallengeDataState challengeDataState = ChallengeDataState.mainInstance;
 
   void generateNewChallenges() {
-    for (int i = 0; i < 5; i++) {
-        challengeDataState.addChallengeShown(_challenges[
-            random.nextInt(_challenges.length)]);
-    }
-
+      for (int i = 0; i < 5; i++) {
+          challengeDataState.addChallengeShown(_challenges[
+              random.nextInt(_challenges.length)]);
+      }
   }
   
 
